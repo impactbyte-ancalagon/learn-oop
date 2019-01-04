ff7000c7308037e890e66027fe0fcc901b6c22eb
M Haidar Hanif <git@mhaidarhanif.com>
2019-01-04 14:35:39 +0700

    Implement extends

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const formatter = new Intl.NumberFormat("id-ID", {
+  style: "currency",
+  currency: "IDR"
+})
+
 class Human {
   constructor(name, age) {
     this.name = name
@@ -13,10 +18,24 @@ class Human {
   }
 }
 
-const haidar = new Human("Haidar", 25)
-const haekal = new Human("Haekal", 25)
-const fadhil = new Human("Fadhil", 14)
+class Employee extends Human {
+  constructor(name, age, profession, salary) {
+    super(name, age)
+    this.profession = profession
+    this.salary = salary
+  }
 
-console.log(haidar.getNameAndAge())
-console.log(haekal.getNameAndAge())
-console.log(fadhil.getNameAndAge())
+  getSalary() {
+    return `${formatter.format(this.salary)}`
+  }
+
+  updateSalary(newSalary) {
+    this.salary = newSalary
+  }
+}
+
+const gatot = new Employee("Gatot Lagi", 21, "Developer", 9000000)
+const ipul = new Employee("Ipul", 26, "Programmer", 12000000)
+
+gatot.updateSalary(120000000)
+console.log(gatot.getSalary())
